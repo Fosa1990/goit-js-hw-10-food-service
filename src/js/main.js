376255b968ba41becdac1ed77f332c180e8326cf
Fosa1990 <fosa1990@gmail.com>
2021-09-22 15:57:12 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
-import menuData from '../menu.json';
+import menuData from '../json/menu.json';
 import menuTemplate from '../templates/card.hbs';
+import { bodyEl, themeSwitchEL, menuEL } from './refs.js';
+import { LIGHT, DARK } from './theme';
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+let uiTheme = localStorage.getItem('ui-theme');
+if (!uiTheme) {
+  uiTheme = LIGHT;
+  localStorage.setItem('ui-theme', LIGHT);
+}
+
+bodyEl.classList.add(uiTheme);
+themeSwitchEL.checked = uiTheme === LIGHT ? false : true;
+
+const menuMarkup = menuTemplate(menuData);
+menuEL.insertAdjacentHTML('beforeend', menuMarkup);
+
+themeSwitchEL.addEventListener('change', fnChangeTheme);
+
+function fnChangeTheme(event) {
+  bodyEl.classList.toggle(DARK);
+  bodyEl.classList.toggle(LIGHT);
+  localStorage.setItem('ui-theme', event.target.checked ? DARK : LIGHT);
+}
